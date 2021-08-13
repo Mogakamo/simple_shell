a2f424f6e6e03c3037a91136f10188d878aca6c2
Mogakamo <mogaka.amo254@gmail.com>
2021-08-13 23:03:27 +0300

    Updated the AUTHORS file

--- scripts/generate-authors.sh
+++ scripts/generate-authors.sh
@@ -5,9 +5,10 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
 {
 	cat <<- 'EOH'
-		#This file lists all the individuals having contributed to the repository
-	
+		# This file lists all the individuals having contributed to the repository
+	# Do not hard code your details here!!
+
 	EOH
 	echo
-	git log --format='%aN <%aE' | LC_ALL=C.UTF-8 sort -uf
+	git log --format='%aN <%aE' | LC_ALL=C.UTF-8 sort -uf 
 } > AUTHORS
